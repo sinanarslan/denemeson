b85ab602173eb7df153c7cf4cd1308bfd59b9780
BIGUSOFThttps://github.com/sinanarslan/bigusoft.git <BIGUSOFT@DESKTOP-PKRC611>
2018-09-25 21:50:02 +0300

    de
    Değiştir

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Selam");
         }
 
         public ActionResult About()
